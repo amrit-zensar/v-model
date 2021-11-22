a375c3d4e15d694cd4b89d61cf6848b9bc77b4c4
Amrit Kumar Sutar <amrit.sutar@zensar.com>
2021-11-22 16:38:49 +0530

    first commit

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="hello">
+  <!-- <div class="hello">
     <h1>{{ msg }}</h1>
     <p>
       For a guide and recipes on how to configure / customize this project,<br>
@@ -27,6 +27,13 @@
       <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
       <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
     </ul>
+  </div> --> 
+
+  <div id="app">
+      <h1>{{ message }} {{ message2 }}</h1>
+      <input type="text" v-model="message" />
+      <!-- <h1>{{ message2 }}</h1> -->
+      <input type="text" v-model="message2" />
   </div>
 </template>
 
@@ -35,12 +42,18 @@ export default {
   name: 'HelloWorld',
   props: {
     msg: String
+  },
+  data() {
+    return {message: 'Hello VUEJS', message2:'V-Model Binding'};
   }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+input{
+  margin: 20px;
+}
 h3 {
   margin: 40px 0 0;
 }
